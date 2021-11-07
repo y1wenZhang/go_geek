11274950eb7183cb98e74738005765f790b0733b
zhanghang <y1wenzhang@yeah.net>
2021-11-07 11:46:43 +0800

    第二周作业

---
+++ second_async/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync/errgroup"
+)
+
+func helloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w,"hello world")
+}
+
+func startServer(server *http.Server) error {
+	http.HandleFunc("/hello", helloServer)
+	fmt.Println("start server")
+	err := server.ListenAndServe()
+	return err
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	group, errCtx := errgroup.WithContext(ctx)
+	src := &http.Server{Addr: ":9090"}
+	group.Go(func() error {
+		return startServer(src)
+	})
+
+	group.Go(func() error {
+		<-errCtx.Done()
+		fmt.Println("http server stop")
+		return src.Shutdown(errCtx)
+	})
+	channel := make(chan os.Signal, 1)
+	signal.Notify(channel)
+
+	group.Go(func() error {
+		for {
+			select {
+			case <-errCtx.Done():
+				return errCtx.Err()
+			case <-channel:
+				cancel()
+			}
+		}
+		return nil
+	})
+	if err := group.Wait();err != nil{
+		fmt.Println("group error:", err)
+	}
+	fmt.Println("all group done")
+}
